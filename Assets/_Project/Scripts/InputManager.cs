c6cbc4ebb4b2b722ecedd854e39fcbca23d94c0c
stephanedurette <stephanedurette@rogers.com>
2026-02-12 14:10:53 -0500

    no message

--- Assets/_Project/Scripts/InputManager.cs
+++ Assets/_Project/Scripts/InputManager.cs
@@ -6,8 +6,12 @@ public class InputManager : MonoBehaviour
     enum InputTypes { Mouse, Touchscreen }
 
     [SerializeField] private InputTypes inputType;
+    [SerializeField] private float maxInputDragDistance = 10;
 
-    [SerializeField] private UnityEvent<Vector2> OnRelease;
+    [SerializeField] private UnityEvent<Vector2> OnCursorDown;
+    [SerializeField] private UnityEvent<Vector2> OnCursorUp;
+
+    private Vector2 lastSelectPosition;
 
     private void Update()
     {
@@ -26,15 +30,44 @@ public class InputManager : MonoBehaviour
 
     private void UpdateMouseInput()
     {
+        if (Input.GetMouseButtonDown(0)) { 
+            OnInputDown(Input.mousePosition);
+        }
+
         if (Input.GetMouseButtonUp(0)) { 
-            OnRelease?.Invoke(Input.mousePosition);
+            OnInputUp(Input.mousePosition);
         }
     }
 
     private void UpdateTouchScreenInput()
     {
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) { 
-            OnRelease?.Invoke(Input.GetTouch(0).position);
+        if (Input.touchCount == 0) return;
+
+        Touch touch = Input.GetTouch(0);
+
+        switch (touch.phase){
+            case TouchPhase.Began:
+                OnInputDown(touch.position);
+                break;
+            case TouchPhase.Ended:
+                OnInputUp(touch.position);
+                break;
+            default:
+                break;
+
         }
+    }
+
+    private void OnInputDown(Vector2 inputPosition)
+    {
+        lastSelectPosition = inputPosition;
+        OnCursorDown?.Invoke(lastSelectPosition);
+    }
+
+    private void OnInputUp(Vector2 inputPosition) 
+    {
+        if ((inputPosition - lastSelectPosition).sqrMagnitude > Mathf.Pow(maxInputDragDistance, 2)) return;
+
+        OnCursorUp?.Invoke(lastSelectPosition);
     }
 }
