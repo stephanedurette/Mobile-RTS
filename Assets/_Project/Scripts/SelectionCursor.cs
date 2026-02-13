b6ac6ecf4b4f179dbc83ed43317cd33c03191e27
stephanedurette <stephanedurette@rogers.com>
2026-02-13 14:52:13 -0500

    no message

--- Assets/_Project/Scripts/SelectionCursor.cs
+++ Assets/_Project/Scripts/SelectionCursor.cs
@@ -1,18 +1,44 @@
-using System.Collections;
+using DG.Tweening;
 using UnityEngine;
 
 public class SelectionCursor : MonoBehaviour
 {
-    [SerializeField] private float activeDuration;
+    private SpriteRenderer spriteRenderer;
+
+    float fadeDuration, scaleDuration;
+    Color startColor, endColor;
+    Vector3 startScale, apexScale, endScale;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+
+        fadeDuration = 1f;
+        scaleDuration = 1f;
+
+        startColor = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 1);
+        endColor = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 0);
+
+        startScale = Vector3.one;
+        apexScale = Vector3.one * 1.2f;
+        endScale = Vector3.zero;
+    }
 
     private void OnEnable()
     {
-        StartCoroutine(SetActiveFalseCoroutine());
-    }
+        spriteRenderer.color = startColor;
+        spriteRenderer.transform.localScale = startScale;
 
-    private IEnumerator SetActiveFalseCoroutine()
-    {
-        yield return new WaitForSeconds(activeDuration);
-        gameObject.SetActive(false);
+        //Fade
+        DOTween.Sequence()
+            .Append(DOTween.To(() => spriteRenderer.color, x => spriteRenderer.color = x, endColor, fadeDuration).SetEase(Ease.OutCubic))
+            .AppendCallback(() => gameObject.SetActive(false))
+            .Play();
+
+        //Scale
+        DOTween.Sequence()
+            .Append(DOTween.To(() => spriteRenderer.transform.localScale, x => spriteRenderer.transform.localScale = x, apexScale, scaleDuration * .2f).SetEase(Ease.InCubic))
+            .Append(DOTween.To(() => spriteRenderer.transform.localScale, x => spriteRenderer.transform.localScale = x, endScale, scaleDuration * .8f).SetEase(Ease.OutCubic))
+            .Play();
     }
 }
